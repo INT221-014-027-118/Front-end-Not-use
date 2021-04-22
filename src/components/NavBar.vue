c774b85c3c0e65dba3c58fbfafb7887ea152068e
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-04-22 17:23:09 +0700

    [Update]Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,23 @@
 <template>
-    <div class="bg-gray-300 fixed w-full top-0 text-black">
+    <div class="fixed w-full top-0 text-indigo-900 bg-gray-300 dark:bg-gray-900 dark:text-gray-100">
     <div class="flex relative max-w-6xl mx-auto h-16 md:h-20 px-4">
         <div class="flex relative items-center">
-            <img alt="Vue logo" src="@/assets/logo.png" class="w-20" />
-        </div>
+              <i class="material-icons text-7xl  p-2 animate-spin">sports_esports</i> 
+              <p class="font-mono p-2 font-bold text-2xl tracking-tighter animate-pulse">Refun Guarntee</p>       
+          </div>
         <div>
         <div class="flex items-center absolute inset-y-0 right-0">
-            <div class="flex font-medium font-bold">
+            <div class="flex font-medium font-bold items-center">
                 <router-link to="/" class="px-4 py-7">Home</router-link>
                 <router-link to="/about" class="px-4 py-7">About</router-link>
                 <p class="border-r-2 border-gray-300 h-5 w-1 my-auto mx-5"></p>
                 <div class="cursor-pointer">
-                <div :class="{ hidden: !change }" @click="switchMode" class="px-4 py-7" >
-                    <i class="material-icons">light_mode</i>
-                </div>
-                <div :class="{ hidden: change }"  @click="switchMode" class="px-4 py-7 " >
-                    <i class="material-icons">dark_mode</i>
-                </div>
+                  <div :class="{ hidden: !change }" @click="switchMode" class="px-4 py-7" >
+                      <i class="material-icons">light_mode</i>
+                  </div>
+                  <div :class="{ hidden: change }"  @click="switchMode" class="px-4 py-7 " >
+                      <i class="material-icons">dark_mode</i>
+                  </div>
                 </div>
             </div>
         </div>
@@ -37,3 +38,7 @@ export default {
   },
 };
 </script>
+
+<style >
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home mt-20">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.svg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
