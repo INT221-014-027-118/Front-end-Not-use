6a826d892b0946a23f0a276055b5ec2866078b2e
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-04-22 23:26:47 +0700

    [Update] Team, Navbar.vue CSS

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,30 +1,32 @@
 <template>
-    <div class="fixed w-full top-0 text-indigo-900 bg-gray-300 dark:bg-gray-900 dark:text-gray-100">
+  <div class="fixed w-full top-0 z-50 text-blue-900 bg-gray-200 dark:bg-gray-900 dark:text-gray-100">
     <div class="flex relative max-w-6xl mx-auto h-16 md:h-20 px-4">
-        <div class="flex relative items-center">
-              <i class="material-icons text-7xl  p-2 animate-spin">sports_esports</i> 
-              <p class="font-mono p-2 font-bold text-2xl tracking-tighter animate-pulse">Refun Guarntee</p>       
+          <div >
+              <router-link to="/"  class="flex relative items-center hover:text-indigo-500 cursor-pointer">
+              <i class="material-icons text-7xl">sports_esports</i> 
+              <span class="font-mono p-2 font-bold text-2xl tracking-tighter animate-pulse hidden md:inline-flex">Refun Guarntee</span>
+              </router-link>       
           </div>
         <div>
-        <div class="flex items-center absolute inset-y-0 right-0">
-            <div class="flex font-medium font-bold items-center">
-                <router-link to="/" class="px-4 py-7">Home</router-link>
-                <router-link to="/team" class="px-4 py-7">Team</router-link>
-                <router-link to="/about" class="px-4 py-7">About</router-link>
-                <p class="border-r-2 border-gray-300 h-5 w-1 my-auto mx-5"></p>
-                <div class="cursor-pointer">
-                  <div :class="{ hidden: !change }" @click="switchMode" class="px-4 py-7" >
-                      <i class="material-icons">light_mode</i>
+          <div class="flex items-center absolute inset-y-0 right-0">
+              <div class="flex font-medium font-bold items-center">
+                  <router-link to="/" class="px-4 py-7 hover:text-indigo-500">Home</router-link>
+                  <router-link to="/team" class="px-4 py-7 hover:text-indigo-500">Team</router-link>
+                  <router-link to="/about" class="px-4 py-7 hover:text-indigo-500">About</router-link>
+                  <p class="border-r-2 dark:border-gray-300 border-indigo-900 h-5 w-1 my-auto mx-5"></p>
+                  <div class="cursor-pointer">
+                    <div :class="{ hidden: !change }" @click="switchMode" class="px-4 py-7 hover:text-indigo-500">
+                        <i class="material-icons">light_mode</i>
+                    </div>
+                    <div :class="{ hidden: change }"  @click="switchMode" class="px-4 py-7 hover:text-indigo-500">
+                        <i class="material-icons">dark_mode</i>
+                    </div>
                   </div>
-                  <div :class="{ hidden: change }"  @click="switchMode" class="px-4 py-7 " >
-                      <i class="material-icons">dark_mode</i>
-                  </div>
-                </div>
-            </div>
-        </div>
+              </div>
+          </div>
         </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="mt-44 flex justify-between mx-44">
-    <div v-for="member in teams" :key="member.studentId">
-      <h3>{{ member.studentId }}</h3>
-      <h3>{{ member.name }}</h3>
-      <h3>roles</h3>
-      <ul v-for="role in member.roles" :key="role" class="flex">
-          <li>{{ role }}</li>
-      </ul>
+  <div class="w-4/5 mx-auto mt-44">
+    <div class="flex flex-col items-stretch sm:flex-row">
+      
+      <div class="sm:w-1/3 p-4" v-for="member in teams" :key="member.studentId">
+        <div class="px-6 py-8 rounded-lg shadow-2xl text-center backdrop-filter backdrop-blur-3xl ">
+          <div class="mb-5">
+              <img class="w-auto mx-auto rounded-full mb-3" src="https://i.pravatar.cc/150?img=66"  alt=""/>
+              <h2 class="text-xl font-medium">{{ member.name }}</h2>
+              <p>{{ member.studentId }}</p>
+              <p class="my-2 pb-2 border-b-2 border-gray-400">ROLE</p>
+              <div class="">
+                  <span class="text-blue-500 block"  v-for="role in member.roles" :key="role" >{{ role }}</span>
+              </div>
+          </div>
+          <a :href="member.github" class="px-4 py-2 bg-blue-500 text-white rounded-full" >Git Hub</a>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -20,16 +30,19 @@ export default {
           studentId: "62130500014",
           name: "Jakkapong Praditthanachot",
           roles: ["Frontend", "DataBase", "Network"],
+          github:"https://github.com/jakkapongqcue"
         },
         {
           studentId: "62130500027",
           name: "Traitawat Jitchana",
           roles: ["Frontend", "DataBase", "Backend"],
+          github:"https://github.com/actlook25957"
         },
         {
           studentId: "62130500118",
           name: "Apisit kaewnongsaeng",
           roles: ["Frontend", "DataBase", "Devops"],
+          github:"https://github.com/Top1568"
         },
       ],
     };
