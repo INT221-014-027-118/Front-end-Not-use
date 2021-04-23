adca1a32615ba5ecf82144caeff51ed8b2f049d4
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-04-23 23:14:26 +0700

    [Add] Home🏠

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,43 @@
 <template>
-  <div class="home mt-20">
-    <img alt="Vue logo" src="../assets/logo.svg">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <router-link to="/form" class="text-8xl">FORM</router-link>
+
+  <div class="w-4/5 mx-auto mt-20 md:mt-44">
+    <div class="flex flex-col items-stretch md:flex-row">
+      
+      <div class="md:w-1/3 p-10 cursor-pointer transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"  >
+        <div class="h-36 rounded-t-lg bg-contain bg-white bg-no-repeat bg-top" style="background-image: url(https://mercular.s3.ap-southeast-1.amazonaws.com/images/products/2019/11/Razer-Kraken-Kitty-Quart-1.jpg)"></div>
+        <div class="flex content-center justify-center px-6 py-6 rounded-b-lg shadow-2xl text-center bg-blue-200 dark:bg-blue-900" >
+              <i class="material-icons p-5 rounded-full bg-blue-700 text-white mr-6">headset_mic</i>
+              <span class="text-xl mt-5 font-mono uppercase ">headset</span>
+        </div>
+      </div>
+
+      <div class="md:w-1/3 p-10 cursor-pointer transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"  >
+        <div class="h-36 rounded-t-lg bg-contain bg-white bg-no-repeat bg-top" style="background-image: url(https://resource.logitechg.com/w_1000,c_limit,q_auto:best,f_auto,dpr_auto/content/dam/gaming/en/products/pro-x-keyboard/pro-x-keyboard-gallery-1.png?v=1)"></div>
+        <div class="flex content-center justify-center px-6 py-6 rounded-b-lg shadow-2xl text-center bg-blue-200 dark:bg-blue-900" >
+              <i class="material-icons p-5 rounded-full bg-blue-700 text-white mr-6">keyboard</i>
+              <span class="text-xl mt-5 font-mono uppercase ">keyboard</span>
+        </div>
+      </div>
+
+      <div class="md:w-1/3 p-10 cursor-pointer transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"  >
+        <div class="h-36 rounded-t-lg bg-contain bg-white bg-no-repeat bg-top" style="background-image: url(https://compass-ssl.microsoft.com/assets/a7/a5/a7a5f18b-e093-4b19-804f-46d2eae9f14c.jpg?n=Microsoft_Bluetooth_Ergonomic_Mouse_PB.jpg)"></div>
+        <div class="flex content-center justify-center px-6 py-6 rounded-b-lg shadow-2xl text-center bg-blue-200 dark:bg-blue-900" >
+              <i class="material-icons p-5 rounded-full bg-blue-700 text-white mr-6">mouse</i>
+              <span class="text-xl mt-5 font-mono uppercase ">mouse</span>
+        </div>
+      </div>
+
+    </div>
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
