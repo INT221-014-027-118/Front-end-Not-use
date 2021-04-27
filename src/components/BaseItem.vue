8104b68bf210373f9606ba4a7aa10a131ffd0809
Top1568 <apisit.top@mail.kmutt.ac.th>
2021-04-27 15:35:06 +0700

    [Update]Item component

---
+++ src/components/BaseItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col rounded-lg shadow-md">
+    <div class=" overflow-hidden w-full relative pb-72">
+      <img
+        src="@/assets/headset.svg"
+        class="absolute h-full w-full object-cover object-center p-1 rounded-lg bg-white"
+        alt=""
+      />
+    </div>
+    <div class="z-10 w-full ">
+      <div class="py-5 px-5">
+        <h1 class="font-bold text-lg">{{product.name}}</h1>
+        <div class="flex items-center justify-between">
+          <div class="text-sm font-light">Size : Regular</div>
+          <div class="text-2xl text-red-600 font-bold">$ {{product.price}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ props:{
+        product:Object
+    },
+
+};
+</script>
+
+<style></style>
--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -2,106 +2,31 @@
     <div class="h-full items-center mx-auto max-w-6xl bg-gray-200 dark:bg-gray-700 rounded-md mb-10">
         <div class="text-center bg-blue-200 dark:bg-blue-800 px-2 py-3 text-xl font-mono tracking-wider rounded-md">test Title</div>
         
-        <div class="container p-2 mx-auto grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-
-
-            <div class="flex flex-col rounded-lg shadow-md">
-                <div class=" overflow-hidden w-full relative pb-72" >
-                    <img  src="@/assets/headset.svg" class="absolute h-full w-full object-cover object-center p-1 rounded-lg bg-white" alt="" />
-                </div>
-                <div class="z-10 w-full ">
-                    <div class="py-5 px-5">
-                    <h1 class="font-bold text-lg">Product name</h1>
-                    <span class="font-bold text-lg">Model</span>
-                    <div class="flex items-center justify-between">
-                        <div class="text-sm font-light">Size : Regular</div>
-                        <div class="text-2xl text-red-600 font-bold">$ 8.00</div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="flex flex-col rounded-lg  shadow-md">
-                <div class=" overflow-hidden w-full relative pb-72" >
-                    <img  src="@/assets/keyboard.svg" class="absolute h-full w-full object-cover object-center p-1 rounded-lg bg-white" alt="" />
-                </div>
-                <div class="z-10 w-full ">
-                    <div class="py-5 px-5">
-                    <h1 class="font-bold text-lg">Product name</h1>
-                    <span class="font-bold text-lg">Model</span>
-                    <div class="flex items-center justify-between">
-                        <div class="text-sm font-light">Size : Regular</div>
-                        <div class="text-2xl text-red-600 font-bold">$ 8.00</div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="flex flex-col rounded-lg  shadow-md">
-                <div class=" overflow-hidden w-full relative pb-72" >
-                    <img  src="@/assets/mouse.svg" class="absolute h-full w-full object-cover object-center p-1 rounded-lg bg-white" alt="" />
-                </div>
-                <div class="z-10 w-full ">
-                    <div class="py-5 px-5">
-                    <h1 class="font-bold text-lg">Product name</h1>
-                    <span class="font-bold text-lg">Model</span>
-                    <div class="flex items-center justify-between">
-                        <div class="text-sm font-light">Size : Regular</div>
-                        <div class="text-2xl text-red-600 font-bold">$ 8.00</div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-
-                    <div class="flex flex-col rounded-lg  shadow-md">
-                <div class=" overflow-hidden w-full relative pb-72" >
-                    <img  src="@/assets/headset.svg" class="absolute h-full w-full object-cover object-center p-1 rounded-lg bg-white" alt="" />
-                </div>
-                <div class="z-10 w-full ">
-                    <div class="py-5 px-5">
-                    <h1 class="font-bold text-lg">Product name</h1>
-                    <span class="font-bold text-lg">Model</span>
-                    <div class="flex items-center justify-between">
-                        <div class="text-sm font-light">Size : Regular</div>
-                        <div class="text-2xl text-red-600 font-bold">$ 8.00</div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="flex flex-col rounded-lg  shadow-md">
-                <div class=" overflow-hidden w-full relative pb-72" >
-                    <img  src="@/assets/keyboard.svg" class="absolute h-full w-full object-cover object-center p-1 rounded-lg bg-white" alt="" />
-                </div>
-                <div class="z-10 w-full ">
-                    <div class="py-5 px-5">
-                    <h1 class="font-bold text-lg">Product name</h1>
-                    <span class="font-bold text-lg">Model</span>
-                    <div class="flex items-center justify-between">
-                        <div class="text-sm font-light">Size : Regular</div>
-                        <div class="text-2xl text-red-600 font-bold">$ 8.00</div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="flex flex-col rounded-lg  shadow-md">
-                <div class=" overflow-hidden w-full relative pb-72" >
-                    <img  src="@/assets/mouse.svg" class="absolute h-full w-full object-cover object-center p-1 rounded-lg bg-white" alt="" />
-                </div>
-                <div class="z-10 w-full ">
-                    <div class="py-5 px-5">
-                    <h1 class="font-bold text-lg">Product name</h1>
-                    <span class="font-bold text-lg">Model</span>
-                    <div class="flex items-center justify-between">
-                        <div class="text-sm font-light">Size : Regular</div>
-                        <div class="text-2xl text-red-600 font-bold">$ 8.00</div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-
+        <div class="container p-2 mx-auto grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3" >
+            <base-item :product="item" v-for="item in items" :key="item.id"></base-item>
         </div>
     </div>
 
 </template>
+
+<script>
+import BaseItem from "@/components/BaseItem.vue"
+
+export default {
+  name: 'Home',
+  components: {
+    BaseItem,
+  },
+  data() {
+    return {
+      items:[]
+    }
+  },
+  async created() {
+        fetch('http://localhost:5000/products')
+        .then((res) => res.json())
+        .then((data) => this.items = data)
+        .catch((error) => console.log(error));
+  },
+}
+</script>
