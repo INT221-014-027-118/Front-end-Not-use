6bfa5e1be4ee475fb7e20ced54170de0769f86f8
Top1568 <apisit.top@mail.kmutt.ac.th>
2021-04-29 00:01:36 +0700

    [Update]Digest type

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,19 +1,27 @@
 <template>
-<div class="mt-24">
-  <div class="h-full items-center mx-auto max-w-6xl bg-blue-100 dark:bg-gray-700 rounded-md mb-8" v-for="brand in brandsObjs" :key="brand.brand">
-    <div class="text-center bg-blue-200 dark:bg-blue-800 px-2 py-3 text-xl font-mono tracking-wider rounded-md">
-      {{brand.brand}}
-    </div>
+  <div class="mt-24">
+    <div
+      class="h-full items-center mx-auto max-w-6xl bg-blue-100 dark:bg-gray-700 rounded-md mb-8"
+      v-for="brand in brandsObjs"
+      :key="brand.brand"
+    >
+      <div
+        class="text-center bg-blue-200 dark:bg-blue-800 px-2 py-3 text-xl font-mono tracking-wider rounded-md"
+      >
+        {{ brand.brand }}
+      </div>
 
-    <div class="container p-2 mx-auto grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3" >
-      <base-item
-        :product="item"
-        v-for="item in brand.items"
-        :key="item.id"
-      ></base-item>
+      <div
+        class="container p-2 mx-auto grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+      >
+        <base-item
+          :product="item"
+          v-for="item in brand.items"
+          :key="item.id"
+        ></base-item>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -38,14 +46,20 @@ export default {
     async getProducts() {
       await fetch("http://localhost:5000/products")
         .then((res) => res.json())
-        .then(
-          (data) =>
-            (this.items = data.sort((a, b) => {
-              if (a.brand > b.brand) return 1;
-              if (a.brand < b.brand) return -1;
-              return 0;
-            }))
-        )
+        .then((data) => {
+          this.items = data.sort((a, b) => {
+            if (a.brand > b.brand) return 1;
+            if (a.brand < b.brand) return -1;
+            return 0;
+          });
+        })
+        .then(() => {
+          if (!this.resetPage) {
+            this.items = this.items.filter((item) => {
+              return item.type.toLowerCase() === this.type.toLowerCase();
+            });
+          }
+        })
         .catch((error) => console.log(error));
     },
   },
