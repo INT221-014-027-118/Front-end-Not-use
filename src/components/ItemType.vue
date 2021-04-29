bead432ade48c14bedbd60c3ce875133069db808
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-04-29 11:08:27 +0700

    [Update] Home poduct type & Navbar

--- src/components/ItemType.vue
+++ src/components/ItemType.vue
@@ -8,7 +8,7 @@
             <div class="h-28 md:h-32 lg:h-52 rounded-t-lg bg-contain bg-white bg-no-repeat bg-top"  :style="{ backgroundImage: `url(${item.image})` }"></div>
             <div class="flex justify-center items-center p-3 sm:p-6 rounded-b-lg shadow-2xl text-center bg-blue-200 dark:bg-blue-900" >
                 <i class="material-icons p-3 md:p-3 lg:p-6 rounded-full bg-blue-700 text-white mr-3">{{item.icon}}</i>
-                <span class="text-xl font-mono uppercase">{{item.text}}</span>
+                <span class="text-xl font-mono uppercase">{{item.itmeName}}</span>
             </div>
         </router-link>
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
 
-<div class="flex items-center md:h-screen lg:mt-0 -mt-5">
+<div class="flex flex-col h-screen justify-center my-20 md:my-0">
     <div class="w-4/5 mx-auto mt-24 sm:mt-44 md:mt-0">
         <div class="flex flex-col items-stretch md:flex-row">
             <item-type v-for="item in itemType" :key="item.img" :item="item" ></item-type>
         </div>
+    </div>
+    <div class="flex justify-center mt-5 md:mt-20">
+      <router-link to="/product" class="px-5 py-2 rounded-lg shadow ring-4  bg-blue-200 dark:bg-blue-900 hover:bg-blue-300 transition duration-200 transform dark:hover:bg-blue-700">All Products</router-link>
     </div>
 </div>
 
@@ -28,19 +31,16 @@ export default {
           itmeName:"headset",
           image: headset,
           icon:"headset_mic",
-          text:"headset"  
         },
         {
           itmeName:"keyboard",
           image: keyboard,
           icon:"keyboard",
-          text:"keyboard"  
         },
         {
           itmeName:"mouse",
           image: mouse,
           icon:"mouse",
-          text:"mouse"  
         },
       ]
     }
