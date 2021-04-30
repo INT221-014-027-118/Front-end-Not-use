ff42c0cd32667185635334d1b20ab85bcd6e8414
Top1568 <apisit.top@mail.kmutt.ac.th>
2021-04-30 23:17:02 +0700

    [Update]Delete

--- src/components/BaseItem.vue
+++ src/components/BaseItem.vue
@@ -3,13 +3,11 @@
     class="flex flex-col rounded-lg shadow-md "
     @mouseenter="showED = !showED"
     @mouseleave="showED = !showED"
-    
   >
     <div class=" overflow-hidden w-full relative pb-72">
       <div
         v-show="showED"
         class="flex  space-x-8 absolute z-10 top-1/2 right-1/2 transform  translate-x-2/4"
-        
       >
         <div
           class=" bg-green-500 py-4 px-5 cursor-pointer rounded-full text-white shadow-2xl"
@@ -28,7 +26,7 @@
         :src="image"
         class="absolute h-full w-full object-cover object-center p-1 rounded-lg bg-white"
         alt=""
-        :class="{ 'filter opacity-50' : showED}"
+        :class="{ 'filter opacity-50': showED }"
       />
       <div
         class=" absolute -mt-4 w-full p-1 bottom-0 bg-gray-500 opacity-60 h-8 rounded-b-lg"
@@ -82,6 +80,7 @@ export default {
       ],
       image: "",
       showED: false,
+      urlItem: "http://localhost:5000/products/" + this.product.id,
     };
   },
   methods: {
@@ -89,7 +88,14 @@ export default {
       this.$router.push({ name: "Form", params: { itemId: this.product.id } });
     },
     deleteItem() {
-      console.log("del");
+      let confirm = window.confirm("Are you sure?");
+      if (confirm) {
+        fetch(this.urlItem, { method: "DELETE" })
+          .then(() => {
+            this.close();
+          })
+          .catch((error) => console.log(error));
+      }
     },
   },
 
