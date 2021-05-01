896622d139a6cd3ad7ea6f3d7505bfcf187147c6
Top1568 <apisit.top@mail.kmutt.ac.th>
2021-05-01 18:26:35 +0700

    [Update]Delete realtime

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,28 +1,13 @@
 <template>
-    <router-view
-        class="z-40 h-screen w-full backdrop-filter backdrop-blur-xl"
-    />
+    <router-view class="z-40 h-screen w-full backdrop-filter backdrop-blur-xl" />
     <div class="mt-16 md:mt-20 mb-52 md:mb-60">
-        <div
-            class="h-full items-center mx-auto max-w-6xl bg-blue-100 dark:bg-gray-700 rounded-md mb-8 relative"
-            v-for="brand in brandsObjs"
-            :key="brand.brand"
-        >
-            <div
-                class="text-center bg-blue-300 dark:bg-blue-800 px-2 py-3 text-xl font-mono tracking-wider rounded-md sticky top-16 md:top-20 z-30"
-            >
+        <div class="h-full items-center mx-auto max-w-6xl bg-blue-100 dark:bg-gray-700 rounded-md mb-8 relative" v-for="brand in brandsObjs" :key="brand.brand">
+            <div class="text-center bg-blue-300 dark:bg-blue-800 px-2 py-3 text-xl font-mono tracking-wider rounded-md sticky top-16 md:top-20 z-30">
                 {{ brand.brand }}
             </div>
 
-            <div
-                class="container p-2 mx-auto grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
-            >
-                <base-item
-                    :product="item"
-                    v-for="item in brand.items"
-                    :key="item.id"
-                    @del-item="deleteItem"
-                />
+            <div class="container p-2 mx-auto grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                <base-item :product="item" v-for="item in brand.items" :key="item.id" @del-item="deleteItem" />
             </div>
         </div>
     </div>
@@ -38,7 +23,6 @@ export default {
     },
     props: {
         type: String,
-        getAll: String,
     },
     data() {
         return {
@@ -47,9 +31,17 @@ export default {
         };
     },
     methods: {
-        deleteItem(index) {
-            console.log(this.brandsObjs);
-            console.log(index);
+        deleteItem(product) {
+            let brandDeleted = this.brandsObjs.findIndex((item) => {
+                return product.brand === item.brand;
+            });
+            let itemDeleted = this.brandsObjs[brandDeleted].items.findIndex((item) => {
+                return item.id === product.id;
+            });
+            this.brandsObjs[brandDeleted].items.splice(itemDeleted, 1);
+            if (this.brandsObjs[brandDeleted].items.length == 0) {
+                this.brandsObjs.splice(brandDeleted, 1);
+            }
         },
         async getProducts() {
             await fetch("http://localhost:5000/products")
@@ -62,14 +54,9 @@ export default {
                     });
                 })
                 .then(() => {
-                    if (!this.resetPage) {
-                        this.items = this.items.filter((item) => {
-                            return (
-                                item.type.toLowerCase() ===
-                                this.type.toLowerCase()
-                            );
-                        });
-                    }
+                    this.items = this.items.filter((item) => {
+                        return item.type.toLowerCase() === this.type.toLowerCase();
+                    });
                 })
                 .catch((error) => console.log(error));
         },
