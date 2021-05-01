a467afcab6e54e7b63ad1f61e999d9e0a2760ed3
Jakkapong Praidtthanachot <jakkapong.q@mail.kmutt.ac.th>
2021-05-01 16:56:44 +0700

    [Update] UI baesitem

--- src/components/BaseItem.vue
+++ src/components/BaseItem.vue
@@ -1,153 +1,167 @@
 <template>
-  <div
-    class="flex flex-col rounded-lg shadow-xl "
-    @mouseenter="showED = !showED"
-    @mouseleave="showED = !showED"
-    v-show="!isDeleted"
-  >
-    <div class="overflow-hidden w-full relative pb-72">
-      <div
-        v-show="showED"
-        class="flex invisible sm:visible space-x-8 absolute z-10 top-1/2 right-1/2 transform translate-x-2/4"
-      >
-        <div
-          class=" bg-green-500 py-4 px-5 cursor-pointer rounded-full text-white shadow-2xl"
-          @click="editItem"
-        >
-          <span class="material-icons text-4xl">edit</span>
-        </div>
-        <div
-          class=" bg-red-600 py-4 px-5 cursor-pointer rounded-full text-white shadow-2xl"
-          @click="deleteItem"
-        >
-          <span class="material-icons text-4xl">delete</span>
-        </div>
-      </div>
-      <div
-        class="flex visible sm:invisible space-x-8 absolute z-10 right-1/2 transform  translate-x-2/4"
-      >
-        <div
-          class=" flex items-center bg-green-500 py-1 px-5 cursor-pointer rounded-md text-white shadow-2xl"
-          @click="editItem"
-        >
-          <span class="material-icons text-2xl">edit</span>Edit
-        </div>
-        <div
-          class=" flex items-center bg-red-600 py-1 px-5 cursor-pointer rounded-md text-white shadow-2xl"
-          @click="deleteItem"
-        >
-          <span class="material-icons text-2xl">delete</span>Delete
-        </div>
-      </div>
-      <img
-        :src="image"
-        class="absolute h-full w-full object-cover object-center p-1 rounded-t-lg bg-white"
-        alt=""
-        :class="{ ' sm:opacity-50': showED }"
-      />
-      <div
-        class=" absolute -mt-4 w-full p-1 bottom-0 bg-gray-600 opacity-60 h-8"
-      />
-      <div class=" absolute -mt-4 w-full p-1 bottom-0">
-        <div class="flex flex-row-reverse">
-          <div
-            class="w-4 h-4 m-1 rounded-md"
-            v-for="color in product.colors"
-            :key="color.colorId"
-            :style="{ backgroundColor: color.hexColor }"
-          ></div>
-        </div>
-      </div>
-    </div>
-    <router-link
-      :to="{
-        name: 'Item',
-        params: {
-          type: product.type,
-          itemName: product.name,
-          itemId: product.id,
-          itemImgTest: image,
-        },
-      }"
-      class="hover:text-blue-600"
-      @mouseenter="more = !more"
-      @mouseleave="more = !more"
-    >
-      <div class="z-10 w-full relative">
-        <div class="pt-4 pb-8 px-5 bg-blue-200 dark:bg-gray-600 rounded-b-md">
-          <h1
-            class="font-bold text-lg underline sm:no-underline"
-            :class="{ underline: more }"
-          >
-            {{ product.name }}
-          </h1>
-          <div class="flex items-center justify-between">
-            <div class="text-sm font-light text-black dark:text-white ">
-              Warranty : [ ]
-            </div>
-            <div class="text-2xl text-red-600 font-bold">
-              ฿ {{ product.price }}
-            </div>
-          </div>
-          <div
-            class="h-24 p-2 overflow-hidden text-black dark:text-white relative"
-          >
-            {{ product.description }}
+    <div class="flex flex-col rounded-lg shadow-xl" v-show="!isDeleted">
+        <div class="overflow-hidden w-full relative pb-72">
             <div
-              class="absolute z-10 bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-blue-200 dark:from-gray-600  pointer-events-none"
-            ></div>
-          </div>
+                v-show="btnEditer"
+                class="flex space-x-2 sm:space-x-8 absolute z-10 top-1/4 right-1/2 transform translate-x-2/4 translate-y-1/2"
+            >
+                <div
+                    class="bg-green-500 py-4 px-5 cursor-pointer rounded-full text-white shadow-2xl flex items-center"
+                    @click="editItem"
+                >
+                    <span class="material-icons text-4xl">edit</span>Edit
+                </div>
+                <div
+                    class="bg-red-600 py-4 px-5 cursor-pointer rounded-full text-white shadow-2xl flex items-center"
+                    @click="deleteItem"
+                >
+                    <span class="material-icons text-4xl">delete</span>Delete
+                </div>
+            </div>
+            <div>
+                <div
+                    class="material-icons items-center bg-gray-400 py-2 px-3 cursor-pointer absolute z-10 top-2 right-2 rounded-md text-white shadow-2xl"
+                    v-show="!btnEditer"
+                    @click="btnEditer = !btnEditer"
+                >
+                    edit
+                </div>
+                <div
+                    class="material-icons items-center bg-gray-400 py-2 px-3 cursor-pointer absolute z-10 top-2 right-2 rounded-md text-white shadow-2xl"
+                    v-show="btnEditer"
+                    @click="btnEditer = !btnEditer"
+                >
+                    close
+                </div>
+            </div>
+            <img
+                :src="image"
+                class="absolute h-full w-full object-cover object-center p-1 rounded-t-lg bg-white"
+                alt="img product"
+                :class="[btnEditer ? ' opacity-50' : '']"
+            />
+            <div
+                class="absolute -mt-4 w-full p-1 bottom-0 bg-gray-600 opacity-60 h-8"
+            />
+            <div class="absolute -mt-4 w-full p-1 bottom-0">
+                <div class="flex flex-row-reverse">
+                    <div
+                        class="w-4 h-4 m-1 rounded-md"
+                        v-for="color in product.colors"
+                        :key="color.colorId"
+                        :style="{
+                            backgroundColor: color.hexColor,
+                        }"
+                    ></div>
+                </div>
+            </div>
         </div>
-        <div
-          class="absolute right-1/2 transform underline translate-x-1/2 bottom-1 z-10"
+        <router-link
+            :to="{
+                name: 'Item',
+                params: {
+                    type: product.type,
+                    itemName: product.name,
+                    itemId: product.id,
+                    itemImgTest: image,
+                },
+            }"
+            class="hover:text-blue-600"
+            @mouseenter="more = !more"
+            @mouseleave="more = !more"
         >
-          Show More
-        </div>
-      </div>
-    </router-link>
-  </div>
+            <div class="z-10 w-full relative">
+                <div
+                    class="pt-4 pb-8 px-5 bg-blue-200 dark:bg-gray-600 rounded-b-md"
+                >
+                    <h1
+                        class="font-bold text-lg underline sm:no-underline"
+                        :class="{ underline: more }"
+                    >
+                        {{ product.name }}
+                    </h1>
+                    <div class="flex items-center justify-between">
+                        <div
+                            class="text-sm font-light text-black dark:text-white"
+                        >
+                            Warranty : [ ]
+                        </div>
+                        <div class="text-2xl text-red-600 font-bold">
+                            ฿ {{ product.price }}
+                        </div>
+                    </div>
+                    <div
+                        class="h-24 p-2 overflow-hidden text-black dark:text-white relative"
+                    >
+                        {{ product.description }}
+                        <div
+                            class="absolute z-10 bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-blue-200 dark:from-gray-600 pointer-events-none"
+                        ></div>
+                    </div>
+                </div>
+                <div
+                    class="absolute right-1/2 transform underline translate-x-1/2 bottom-1 z-10"
+                >
+                    Show More
+                </div>
+            </div>
+        </router-link>
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      forTestImg: [
-        { type: "keyboard", img: require("@/assets/keyboard.svg") },
-        { type: "headset", img: require("@/assets/headset.svg") },
-        { type: "mouse", img: require("@/assets/mouse.svg") },
-      ],
-      image: "",
-      showED: false,
-      urlItem: "http://localhost:5000/products/" + this.product.id,
-      isDeleted: false,
-    };
-  },
-  methods: {
-    editItem() {
-      this.$router.push({ name: "Form", params: { itemId: this.product.id } });
+    data() {
+        return {
+            forTestImg: [
+                {
+                    type: "keyboard",
+                    img: require("@/assets/keyboard.svg"),
+                },
+                {
+                    type: "headset",
+                    img: require("@/assets/headset.svg"),
+                },
+                {
+                    type: "mouse",
+                    img: require("@/assets/mouse.svg"),
+                },
+            ],
+            image: "",
+            hoverEditer: false,
+            btnEditer: false,
+            urlItem: "http://localhost:5000/products/" + this.product.id,
+            isDeleted: false,
+        };
     },
-    deleteItem() {
-      let confirm = window.confirm("Are you sure?");
-      if (confirm) {
-        fetch(this.urlItem, { method: "DELETE" })
-          .then(() => {
-            this.close();
-          })
-          .catch((error) => console.log(error));
-        this.isDeleted = true;
-      }
+    methods: {
+        editItem() {
+            this.$router.push({
+                name: "Form",
+                params: { itemId: this.product.id },
+            });
+        },
+        deleteItem() {
+            let confirm = window.confirm("Are you sure?");
+            if (confirm) {
+                fetch(this.urlItem, { method: "DELETE" })
+                    .then(() => {
+                        this.close();
+                    })
+                    .catch((error) => console.log(error));
+                this.isDeleted = true;
+            }
+        },
     },
-  },
 
-  props: {
-    product: Object,
-  },
-  created() {
-    this.image = this.forTestImg.find((item) => {
-      return this.product.type.toLowerCase() === item.type.toLowerCase();
-    }).img;
-  },
+    props: {
+        product: Object,
+    },
+    created() {
+        this.image = this.forTestImg.find((item) => {
+            return this.product.type.toLowerCase() === item.type.toLowerCase();
+        }).img;
+    },
 };
 </script>
 
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,78 +1,92 @@
 <template>
-  <router-view class="z-40 h-screen w-full backdrop-filter backdrop-blur-xl"/>
-  <div class="mt-16 md:mt-20 mb-52 md:mb-60">
-    <div class="h-full items-center mx-auto max-w-6xl bg-blue-100 dark:bg-gray-700 rounded-md mb-8 relative"
-      v-for="brand in brandsObjs" :key="brand.brand" >
-      <div class="text-center bg-blue-200 dark:bg-blue-800 px-2 py-3 text-xl font-mono tracking-wider rounded-md sticky top-16 md:top-20 z-30 ">
-        {{ brand.brand }}
-      </div>
+    <router-view
+        class="z-40 h-screen w-full backdrop-filter backdrop-blur-xl"
+    />
+    <div class="mt-16 md:mt-20 mb-52 md:mb-60">
+        <div
+            class="h-full items-center mx-auto max-w-6xl bg-blue-100 dark:bg-gray-700 rounded-md mb-8 relative"
+            v-for="brand in brandsObjs"
+            :key="brand.brand"
+        >
+            <div
+                class="text-center bg-blue-300 dark:bg-blue-800 px-2 py-3 text-xl font-mono tracking-wider rounded-md sticky top-16 md:top-20 z-30"
+            >
+                {{ brand.brand }}
+            </div>
 
-      <div class="container p-2 mx-auto grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3" >
-        <base-item :product="item" v-for="item in brand.items" :key="item.id" />
-      </div>
-
+            <div
+                class="container p-2 mx-auto grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+            >
+                <base-item
+                    :product="item"
+                    v-for="item in brand.items"
+                    :key="item.id"
+                />
+            </div>
+        </div>
     </div>
-  </div>
-
 </template>
 
 <script>
 import BaseItem from "@/components/BaseItem.vue";
 
 export default {
-  name: "Product",
-  components: {
-    BaseItem,
-  },
-  props: {
-    type: String,
-    getAll: String,
-  },
-  data() {
-    return {
-      items: [],
-      brandsObjs: [],
-    };
-  },
-  methods: {
-    async getProducts() {
-      await fetch("http://localhost:5000/products")
-        .then((res) => res.json())
-        .then((data) => {
-          this.items = data.sort((a, b) => {
-            if (a.brand > b.brand) return 1;
-            if (a.brand < b.brand) return -1;
-            return 0;
-          });
-        })
-        .then(() => {
-          if (!this.resetPage) {
-            this.items = this.items.filter((item) => {
-              return item.type.toLowerCase() === this.type.toLowerCase();
-            });
-          }
-        })
-        .catch((error) => console.log(error));
+    name: "Product",
+    components: {
+        BaseItem,
     },
-  },
-  async created() {
-    await this.getProducts();
+    props: {
+        type: String,
+        getAll: String,
+    },
+    data() {
+        return {
+            items: [],
+            brandsObjs: [],
+        };
+    },
+    methods: {
+        async getProducts() {
+            await fetch("http://localhost:5000/products")
+                .then((res) => res.json())
+                .then((data) => {
+                    this.items = data.sort((a, b) => {
+                        if (a.brand > b.brand) return 1;
+                        if (a.brand < b.brand) return -1;
+                        return 0;
+                    });
+                })
+                .then(() => {
+                    if (!this.resetPage) {
+                        this.items = this.items.filter((item) => {
+                            return (
+                                item.type.toLowerCase() ===
+                                this.type.toLowerCase()
+                            );
+                        });
+                    }
+                })
+                .catch((error) => console.log(error));
+        },
+    },
+    async created() {
+        await this.getProducts();
 
-    let brands = new Set(
-      this.items.map((item) => {
-        return item.brand;
-      })
-    );
-    brands = Array.from(brands);
+        let brands = new Set(
+            this.items.map((item) => {
+                return item.brand;
+            })
+        );
+        brands = Array.from(brands);
 
-    brands.forEach((brand) => {
-      this.brandsObjs.push({
-        brand: brand,
-        items: this.items.filter((item) => {
-          return item.brand === brand;
-        }),
-      });
-    });
-  },
+        brands.forEach((brand) => {
+            this.brandsObjs.push({
+                brand: brand,
+                items: this.items.filter((item) => {
+                    return item.brand === brand;
+                }),
+            });
+        });
+    },
 };
 </script>
